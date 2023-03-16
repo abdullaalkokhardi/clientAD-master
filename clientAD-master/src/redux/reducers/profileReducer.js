efe1bfeb385a10a5aa081d51973a3e85e5240a56
abdullaalkokhardi <abdulla.alkokhardi@gmail.com>
2023-03-16 13:58:33 +0400

    CLIENT

---
+++ clientAD-master/src/redux/reducers/profileReducer.js
@@ -0,0 +1,25 @@
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "../types";
+
+const initialState = {
+  profile: null, // to hold current user profile
+  profiles: [], // to hold all profiles
+  error: {},
+  loading: true,
+  repos: [], // to hold all github repositories
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_PROFILE:
+    // to retrieve, update, create.
+    case UPDATE_PROFILE:
+      return { ...state, profile: payload, loading: false };
+    case PROFILE_ERROR:
+      return { ...state, error: payload, loading: false, profile: null };
+
+    default:
+      return state;
+  }
+};
+
